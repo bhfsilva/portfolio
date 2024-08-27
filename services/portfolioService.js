5e6fa77d8abfecf5d7eace6a95baa412a8bb1466
Bruno Henrique <bh45237@gmail.com>
2024-08-26 22:57:04 -0300

    adicionada animacao de skeleton loading aos componentes

--- services/portfolioService.js
+++ services/portfolioService.js
@@ -8,55 +8,24 @@ export function getSocialMediaList() {
   ]
 }
 
-export function getJobExperienceList() {
-  return [
-    {
-      organizationLogo: "/static/jobs-experiences/MPConsultoria-logo.png",
-      organizationName: "MPConsultoria",
-      position: "Desenvolvedor Java Júnior",
-      startMonthYear: "Julho/2023",
-      endMonthYear: null,
-      showTaskList: true,
-      skills: [
-        "Java 11",
-        "SpringBoot",
-        "Angular",
-        "MongoDB",
-        "Azure Service Bus (mensageria)",
-        "WebServices SOAP",
-        "Python",
-        "Selenium",
-        "Git",
-        "Azure"
-      ],
-      tasks: [
-        "Participei na criação de uma API e de um software SaaS para obtenção do XML de notas fiscais eletrônicas utilizando Java com SpringBoot para integração com Webservices municipais, desenvolvendo interfaces com Angular e gerenciando dados com MongoDB;",
-        "Participei de um projeto de web scraping (raspagem de dados), criando automações com as bibliotecas Selenium e BeautifulSoup em Python;",
-        "Refatorei e escrevi a documentação do projeto de web scraping, melhorando a escalabilidade e manutenção do código."
-      ]
-    },
-    {
-      organizationLogo: "/static/jobs-experiences/scala-data-center-logo.png",
-      organizationName: "Scala Data Centers",
-      position: "Estagiário em Redes de Computadores e Monitoração",
-      startMonthYear: "Junho/2021",
-      endMonthYear: "Maio/2023",
-      showTaskList: false,
-      skills: [
-        "Python",
-        "HTML",
-        "CSS",
-        "Peakflow API",
-        "Grafana"
-      ],
-      tasks: [
-        "Criei scripts para configuração de ativos de rede, como switches e roteadores;",
-        "Utilizando a linguagem Python, participei do desenvolvimento de um script que, acessando a API da ferramenta Arbor Peakflow, mapeava quais clientes ainda possuíam vínculo com a empresa;",
-        "Utilizando HTML e CSS, desenvolvi uma tela de login responsiva para um site interno;",
-        "Criei gráficos na ferramenta Grafana que mediam o tráfego de dados de equipamentos."
-      ]
-    }
-  ]
+export const jobExperienceMock = {
+  organizationLogo: "Mock Mock Mock Mock Mock Mock Mock Mock",
+  organizationName: "Mock Mock Mock",
+  position: "Mock",
+  startMonthYear: "Mock Mock",
+  endMonthYear: "Mock Mock",
+  showTaskList: false,
+  skills: ["Mock","Mock",],
+  tasks: ["Mock","Mock","Mock"]
+};
+
+export const projectsMock = {
+  id: "Mock",
+  url: "",
+  image: "Mock",
+  name: "Mock",
+  description: "Mock Mock Mock Mock Mock Mock Mock Mock",
+  languages: ["Mock", "Mock"]
 }
 
 //requisicao sendo feita para API customizada do Next.JS para que token nao seja mostrado no chrome dev tools
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,6 +1,14 @@
 import styles from "./projectComponent.module.scss";
 
-export default function Project({url, imageUrl, projectName, description, languagesList, isGridStyle}) {
+export default function Project({
+  url,
+  imageUrl,
+  projectName,
+  description,
+  languagesList,
+  isGridStyle,
+  isLoading
+}) {
   
   const sanitizedProjectName = projectName.replaceAll("-", " ")
 
@@ -34,16 +42,20 @@ export default function Project({url, imageUrl, projectName, description, langua
         custom-container position-relative padding-20-px position-relative display-flex align-items-center gap-20-px border-radius-8-px ${styles.projectComponentBox} 
       `}
       >
-      <img src={imageUrl} alt={`${projectName} project image`} className="width-40-percent min-width-200-px border-radius-4-px"/>
+      { !isLoading ? 
+        <img src={imageUrl} alt={`${projectName} project image`} className="width-40-percent min-width-200-px border-radius-4-px"/>
+        :
+        <div loading={isLoading.toString()} className="width-40-percent min-width-200-px border-radius-4-px height-192-px"/>
+      }
       <div id="content" className={
         `${isGridStyle ? "text-align-center align-items-center" : ""}
         display-flex flex-direction-column gap-10-px width-100-percent height-100-percent justify-content-space-evenly
         `}>
-        <h1 className="width-100-percent text-transform-capitalize font-weight-500">
+        <h1 className="width-100-percent text-transform-capitalize font-weight-500" loading={isLoading.toString()}>
           {sanitizedProjectName}
         </h1>
-        <p className="font-size-1-dot-1-rem line-height-30-px">{description}</p>
-        <div className="font-size-1-dot-1-rem display-flex gap-5-px flex-flow-wrap">
+        <p className="font-size-1-dot-1-rem line-height-30-px" loading={isLoading.toString()}>{description}</p>
+        <div className="font-size-1-dot-1-rem display-flex gap-5-px flex-flow-wrap" loading={isLoading.toString()}>
           <b className="font-weight-600">Tecnologias:</b>
           {languagesList.map((language, index) => (
             <p key={`${index}-language`} className={`${index == languagesList.length - 1 ? "" : styles.slashSeparated}`}>
@@ -51,9 +63,9 @@ export default function Project({url, imageUrl, projectName, description, langua
             </p>
           ))}
         </div>
-      </div>
+      </div>      
       <div className="position-absolute display-flex gap-5-px top--13-px right-30-px">
-        {formattedLanguagesList.map((language, index) => (
+        {!isLoading && formattedLanguagesList.map((language, index) => (
           <div key={`${index}-${projectName}-language`} className="position-relative width-50-px height-80-px">
             <img className="width-30-px top-17-px right--11-px position-relative z-index-1" src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${language}/${language}-original.svg`} alt={`${language} logo`}/>
             <svg className="position-absolute top-2-px right-4-px" width="40" height="74" viewBox="0 0 40 74" fill="none" xmlns="http://www.w3.org/2000/svg">
